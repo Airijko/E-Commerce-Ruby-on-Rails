947dd38ddf89d856ba75ff447c356f631fa0f703
Airijko <airijkoblade@gmail.com>
2023-12-09 23:40:17 -0600

    Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    before_action :initizalize_session
+    before_action :increment_visit_count
+    before_action :load_cart
+
+    def add_to_cart
+        id = params[:id].to_i
+
+        session[:cart] << id unless session[:cart].include?(id)
+        redirect_to root_path
+    end
+
+    private
+
+    def initizalize_session
+        session[:visit_count] ||= 0
+        session[:cart] ||= []
+    end
+
+    def load_cart
+        @cart = Product.find(session[:cart])
+    end
+
+    def increment_visit_count
+        session[:visit_count] += 1
+        @visit_count = session[:visit_count]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :products
+
+  get 'about', to:'products#about'
+  post 'products/add_to_cart/:id', to:'application#add_to_cart', as: 'add_to_cart'
+  delete 'products/remove_from_cart/:id', to:'application#remove_from_cart', as: 'remove_from_cart'
 end
