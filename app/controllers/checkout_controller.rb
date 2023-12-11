5350403aa32e8544f9a9e2efe099933c480b98d6
Airijko <airijkoblade@gmail.com>
2023-12-11 13:54:32 -0600

    Orders

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,15 @@
 class CheckoutController < ApplicationController
     def create
+        @products = Product.find(session[:cart])
+
+        if @product.nil?
+            redirect_to root_path
+            return
+        end
+
+        respond_to do |format|
+            format.js
+        end
 
     end
 
