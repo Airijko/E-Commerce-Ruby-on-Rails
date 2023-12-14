47a9ed0a1b3d89f89c508964f1bb7b006632bab5
Airijko <airijkoblade@gmail.com>
2023-12-13 20:03:30 -0600

    FINAL FIX (I THINK)

--- db/migrate/20231208214138_add_category_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddCategoryToProducts < ActiveRecord::Migration[7.0]
-  def change
-    unless column_exists? :products, :category_id
-      add_reference :products, :category, foreign_key: true
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
 # Product.delete_all
 
+categories = [
+  'Smartphones & Accessories',
+  'Computers & Tablets',
+  'Cameras, Photo & Video',
+  'Headphones & Audio',
+  'TVs & Home Theater',
+  'Smart Home & Networking',
+  'Drones & Tech Toys',
+  'Wearable Technology',
+  'Gaming Consoles & Accessories',
+  'Office Electronics'
+]
+
+categories.each do |category_name|
+  Category.create(name: category_name)
+end
+
 electronics = [
   'Smartphone',
   'Laptop',
@@ -15,20 +32,25 @@ electronics = [
 
 8.times do
   electronics.each do |product_name|
-  product = Product.create(
+    product = Product.new(
     name: product_name,
     description: Faker::Lorem.sentence(word_count: 10),
     price: Faker::Commerce.price(range: 0..500.00, as_string: true),
     stock_quantity: Faker::Number.between(from: 1, to: 100),
     brand: Faker::Company.name,
-    category_id: Faker::Number.between(from: 1, to: 5),
+    category_id: Faker::Number.between(from: 1, to: 10),
     on_sale: Faker::Boolean.boolean
-  )
+    )
 
-  downloaded_image = URI.open("https://source.unsplash.com/random/?#{product_name}-product")
-  product.image.attach(io: downloaded_image, filename: "m-#{product_name}.jpg")
-
-  puts "Product #{product.name} was created successfully."
+    if product.valid?
+      downloaded_image = URI.open("https://source.unsplash.com/random/?#{product_name}-product")
+      product.image.attach(io: downloaded_image, filename: "m-#{product_name}.jpg")
+      product.save
+      puts "Product #{product.name} was created successfully."
+    else
+      puts "Failed to create product: #{product_name}"
+      puts product.errors.full_messages
+    end
   end
 end
 
